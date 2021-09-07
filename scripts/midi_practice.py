fdf1d87f73cd8d87d13844286b94ff32cc75da67
Sadie Allen <sadiela@bu.edu>
2021-09-07 12:55:21 -0400

    random midi sequence generator

--- scripts/midi_practice.py
+++ scripts/midi_practice.py
@@ -68,9 +68,34 @@ def midifile_to_dict(mid):
         'tracks': tracks
     }
 
-def generate_random_midi(filepath):
-    print("GENERATED")
-    return filepath
+def generate_random_midi(filepath, num_notes=10, subdivision=-4, tempo=120):
+    # for now will always write in the key of c, in one octave
+    # notes can range from 16th to whole notes
+    # notes will not play simultaneously
+    # tempo given in bpm
+    # assumes 4/4 time 
+    # subdivision = =-2 means
+    new_mid = pretty_midi.PrettyMIDI() # type=0
+    # create a track and add it to the midi
+    piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
+    piano = pretty_midi.Instrument(program=piano_program)
+
+    quarter_note_duration = tempo/60
+    notes=[60, 62, 64, 65, 67, 69, 71, 72] #, 73, 74, 75, 76, 77, 78, 79, 80] # 64+7, 64+12]
+    note_lengths = [quarter_note_duration*(2**i) for i in list(range(subdivision, 0)) ]
+    print("POSSIBLE NOTE LENGTHS:", note_lengths)
+    last_endtime = 0
+    for note_num in range(num_notes): 
+        cur_pitch = notes[random.randint(0, len(notes)-1)]
+        cur_note_length = note_lengths[random.randint(0, len(note_lengths)-1)]
+        new_note = pretty_midi.Note(velocity=100, pitch=(cur_pitch), start=last_endtime, end=(last_endtime+cur_note_length))
+        piano.notes.append(new_note)
+        last_endtime += cur_note_length
+
+    new_mid.instruments.append(piano)
+
+    # save to .mid file 
+    new_mid.write(filepath)
 
 def midi_to_tensor(filepath, maxlength=720): # default maxlength is 3 minutes 
     # ASSUMES:
@@ -121,28 +146,33 @@ def tensor_to_midi(tensor, desired_filepath):
 ########################
 # FILE/DIRECTORY PATHS #
 ########################
-DATA_DIR = 'C:\\Users\\sadie\\Documents\\BU\\fall_2021\\research\music\\midi_data\\'
-numpyfile = DATA_DIR + 'new_data\\simple_scale.mid'
+DATA_DIR = 'C:\\Users\\sadie\\Documents\\fall2021\\research\\music\\midi_generation\\data\\'
+simple_scale = DATA_DIR + 'simple_scale.mid'
 
 #####################
 # LOAD IN MIDI FILE #
 #####################
 # old file
 #mid = MidiFile(DATA_DIR + 'classical_piano\\tchaikovsky\\ty_april.mid')
-ableton_mid = MidiFile(DATA_DIR + 'new_data\\simple_scale.mid')
+ableton_mid = MidiFile(simple_scale)
 
-play_music(DATA_DIR + 'new_data\\simple_scale.mid')
+random_midi = DATA_DIR + 'random_midi.mid'
+generate_random_midi(random_midi, tempo=150)
+play_music(random_midi)
+print("DONE WITH RANDOM")
 
-filepath = DATA_DIR + 'new_data\\simple_scale.mid'
-tensor = midi_to_tensor(filepath, maxlength=720) # default maxlength is 3 minutes 
+play_music(simple_scale)
+
+numpyfile = DATA_DIR + 'simple_scale_tensor.npy'
+tensor = midi_to_tensor(simple_scale, maxlength=720) # default maxlength is 3 minutes 
 print('SUM OF ALL ELEMENTS:', np.sum(tensor))
 np.save(numpyfile, tensor)
 
-new_midi_filepath = DATA_DIR + 'new_data\\converted_scale.mid'
+new_midi_filepath = DATA_DIR + 'converted_scale.mid'
 tensor_to_midi(tensor, new_midi_filepath)
 # Save tensor to file 
 #print(tensor.shape)
-play_music(DATA_DIR + 'new_data\\converted_scale.mid')
+play_music(new_midi_filepath)
 
 
 '''pretty_scale = pretty_midi.PrettyMIDI(DATA_DIR + 'new_data\\simple_scale.mid')
@@ -189,12 +219,12 @@ for i in range(30):
 new_mid.save(DATA_DIR + 'new_data\\one_note.mid')
 
 # Save track as JSON
-with open(DATA_DIR + 'new_data\\ableton.json', 'w') as outfile:
-     json.dump(midifile_to_dict(ableton_mid), outfile, indent=2)
+#with open(DATA_DIR + 'new_data\\ableton.json', 'w') as outfile:
+#     json.dump(midifile_to_dict(ableton_mid), outfile, indent=2)
 
 # Save track as JSON
-with open(DATA_DIR + 'new_data\\midi_scale.json', 'w') as outfile:
-     json.dump(midifile_to_dict(ableton_mid), outfile, indent=2)
+#with open(DATA_DIR + 'new_data\\midi_scale.json', 'w') as outfile:
+#     json.dump(midifile_to_dict(ableton_mid), outfile, indent=2)
 
 
 #print(json.dumps(midifile_to_dict(new_mid), indent=2))
