a9043e0f8bdc8031ace944831931d03c16e8e82f
Sadie Allen <sadiela@bu.edu>
2021-09-28 11:37:28 -0400

    chunks

--- scripts/midi_practice.py
+++ scripts/midi_practice.py
@@ -41,6 +41,41 @@ buffer = 1024   # number of samples
 pygame.mixer.init(freq, bitsize, channels, buffer)
 pygame.mixer.music.set_volume(0.8) # optional volume 0 to 1.0
 
+def change_tempo(filepath, newfilepath,  maxlength=720, smallest_subdivision=64, target_tempo=120, previous_tempo=None): # default maxlength is 3 minutes 
+    # changes tempo of midi file
+    
+    # get old midi 
+    midi = pretty_midi.PrettyMIDI(filepath)
+    # old tempo
+    if not previous_tempo: 
+        previous_tempo = midi.estimate_tempo()
+    beat_length = 60/previous_tempo # in seconds secs/beat
+    target_beat_length = 60/target_tempo # in seconds secs/beat
+    
+    # create new midi and instrument 
+    new_midi = pretty_midi.PrettyMIDI() # type=0
+    piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
+    piano = pretty_midi.Instrument(program=piano_program)
+    
+    # convert notes to proper tempo
+    if len(midi.instruments) > 1:
+        print("TOO MANY TRACKS! EMPTY TENSOR RETURNED")
+    else:
+        for instrument in midi.instruments:
+            for note in instrument.notes:
+
+                note_length = note.end - note.start 
+                number_of_beats = note_length/beat_length
+
+                new_start_time = note.start
+
+                new_note = pretty_midi.Note(velocity=100, pitch=note.pitch, start=note.start*(target_tempo/60)*(target_beat_length), end=(note.start+number_of_beats*target_beat_length))
+                piano.notes.append(new_note)
+    new_midi.instruments.append(piano)
+
+    # save to .mid file 
+    new_midi.write(newfilepath)
+
 def play_music(midi_filename):
     try:
         # use the midi file you just saved
@@ -162,13 +197,13 @@ print("DONE WITH RANDOM")'''
 
 play_music(simple_scale)
 
-numpyfile = DATA_DIR + 'simple_scale_tensor.npy'
-tensor = midi_to_tensor(simple_scale, maxlength=720) # default maxlength is 3 minutes 
-print('SUM OF ALL ELEMENTS:', np.sum(tensor))
-np.save(numpyfile, tensor)
+#numpyfile = DATA_DIR + 'simple_scale_tensor.npy'
+#tensor = midi_to_tensor(simple_scale, maxlength=720) # default maxlength is 3 minutes 
+#print('SUM OF ALL ELEMENTS:', np.sum(tensor))
+#np.save(numpyfile, tensor)
 
-new_midi_filepath = DATA_DIR + 'converted_scale.mid'
-tensor_to_midi(tensor, new_midi_filepath)
+new_midi_filepath = DATA_DIR + 'speed_up.mid'
+change_tempo(tensor, new_midi_filepath)
 # Save tensor to file 
 #print(tensor.shape)
 play_music(new_midi_filepath)
