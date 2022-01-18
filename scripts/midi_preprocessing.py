7b38f7572256dd89407ff5aa4073c5082c888a8c
Sadie Allen <sadiela@bu.edu>
2022-01-18 11:18:18 -0500

    preprocessing pipeline

--- scripts/midi_preprocessing.py
+++ scripts/midi_preprocessing.py
@@ -16,14 +16,49 @@ from tqdm import tqdm
 import re
 from glob import glob
 import argparse
+from pathlib import Path
 
 from midi_utility import *
 
 if __name__ == "__main__":
+    datapath = PROJECT_DIRECTORY  / 'data' / 'raw_data'  # '..\\midi_data\\full_dataset_midis_normalized\\'
+    processed_datapath = PROJECT_DIRECTORY / 'data' / 'preprocessed_data'
 
+    parser = argparse.ArgumentParser(description='Arguments for preprocessing midis')
+    #parser.add_argument('-l','--lossfunc', help='Choose loss function.', default=True)
+    parser.add_argument('-r', '--rawdata', help='Path to raw MIDI data.', default=datapath)
+    parser.add_argument('-p', '--pdata', help='Path where processed data will be saved', default=processed_datapath)
 
+    args = vars(parser.parse_args())
+
+    rawdir = args['rawdata']
+    procdir = args['pdata']
+
+    # might not need these two lines
+    rawdir = Path(rawdir)
+    procdir = Path(procdir) 
+    
+    # create new directories
+    os.mkdir(procdir / 'separated') # Do I need another / after to make clear its a directory? probably not 
+    os.mkdir(procdir / 'cropped')
+    os.mkdir(procdir / 'tensor')
+    os.mkdir(procdir / 'sparse')
+
+    sep = procdir / 'separated'
+    crop = procdir / 'cropped'
+    tens = procdir / 'tensor'
+    sparse = procdir / 'sparse'
+   
     print("START")
-    midi_directory = PROJECT_DIRECTORY \ 
-    separate_tracks(input_midi_dir, out_dir)
-    crop_midis(input_midi_dir, out_dir)
-    midis_to_tensors(out_dir, midi_dir, subdiv=32, maxnotelength=16, normalize=False)
+    separate_tracks(rawdir, sep)
+    crop_midis(sep, crop)
+    midis_to_tensors(crop, tens, subdiv=32, maxnotelength=16, normalize=False)
+    convert_to_sparse(tens, sparse, del_tensor_dir=True)
+    # empty other dirs
+
+    for f in os.listdir(sep):
+            os.remove(os.path.join(sep, f))
+
+    for f in os.listdir(crop):
+            os.remove(os.path.join(crop, f))
+
