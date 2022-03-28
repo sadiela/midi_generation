fe9503440de65795632ef3e9b04f796ed8212a52
Sadie Allen <sadiela@dhcp-wifi-8021x-155-41-65-214.bu.edu>
2022-03-28 15:43:29 -0400

    updated single

--- scripts/dp_loss/sparse_dp_loss.py
+++ scripts/dp_loss/sparse_dp_loss.py
@@ -82,7 +82,7 @@ def construct_theta_sparse_k_loop(x, x_hat, device):
         #print("k,i,j:", k, i, j)
         if i > 0 and j > 0:
             if (x[:, i-1] == x_hat[:, j-1]).all():
-                theta = torch.add(theta, torch.sparse_coo_tensor([[k_from_ij(i-1,j-1, m,n)],[k_from_ij(i,j, m,n)]], 0.01, (m*n, m*n), device=device)) #theta[k_from_ij(i-1,j-1, m,n)][k_from_ij(i,j, m,n)] = 0
+                theta = torch.add(theta, torch.sparse_coo_tensor([[k_from_ij(i-1,j-1, m,n)],[k]], 0.01, (m*n, m*n), device=device)) #theta[k_from_ij(i-1,j-1, m,n)][k_from_ij(i,j, m,n)] = 0
             else:
                 theta = theta + torch.sparse_coo_tensor([[k_from_ij(i-1,j-1, m,n)],[k]], note_diff(x[:, i-1] ,x_hat[:, j-1]), (m*n, m*n), device=device) #theta[k_from_ij(i-1,j-1, m,n)][k_from_ij(i,j, m,n)] = note_diff(x[:, i-1] ,x_hat[:, j-1]) # replacing; cost depends on ...?
                 grad_theta = grad_theta + add_gradients(k_from_ij(i-1,j-1, m,n), k, j-1, distance_derivative(x[:,i-1]-x_hat[:,j-1]), m,n, device=device)
@@ -221,7 +221,7 @@ class SparseDynamicLossSingle(torch.autograd.Function):
   def forward(ctx, X_hat, X, device):
     # X_hat, X are bigger than we thought...
     # build theta from original data and reconstruction
-    theta, grad_theta_xhat = construct_theta_sparse(X, X_hat, device)
+    theta, grad_theta_xhat = construct_theta_sparse_k_loop(X, X_hat, device)
     theta = theta.coalesce()
     grad_theta_xhat = grad_theta_xhat.coalesce()
     print("GRADTHETA:", torch.count_nonzero(grad_theta_xhat.to_dense()))
@@ -233,13 +233,25 @@ class SparseDynamicLossSingle(torch.autograd.Function):
     #print(grad_L_theta)
     n_2 = grad_L_theta.size()[0]
     #print(n_2)
-    grad_L_x = torch.zeros((X_hat.shape[0], X_hat.shape[1]))
+    '''grad_L_x = torch.zeros((X_hat.shape[0], X_hat.shape[1]))
     for j in range(n_2):
         for k in range(n_2): ##### NOT DONE W THIS PART!!!! ####
             grad_L_theta_val = get_ijth_val(grad_L_theta, j,k)
             if grad_L_theta_val != 0 and has_values(grad_theta_xhat, j,k): #torch.count_nonzero(grad_theta_xhat[j][k]) != 0:
                 cur_grad = grad_L_theta_val *  get_slice(grad_theta_xhat, j,k) # scalar times pxn
-                grad_L_x += cur_grad
+                grad_L_x += cur_grad'''
+    grad_L_x = torch.zeros((X_hat.shape[0], X_hat.shape[1]))
+    grad_L_theta = grad_L_theta.coalesce()
+    nonzero_indices = grad_L_theta.indices()
+    nonzero_vals = grad_L_theta.values()
+    for idx in range(nonzero_indices.size()[1]):
+        j = nonzero_indices[0,idx] 
+        k = nonzero_indices[1,idx] 
+        grad_L_theta_val = nonzero_vals[idx]
+        if has_values(grad_theta_xhat, j,k): #torch.count_nonzero(grad_theta_xhat[j][k]) != 0:
+            cur_grad = grad_L_theta_val *  get_slice(grad_theta_xhat, j,k)# scalar times pxn
+            grad_L_x += cur_grad
+    
 
     #grad =torch.einsum('ij,ijkl->kl', grad.double(), grad_theta.double())
     print('FINAL GRADIENT:', grad_L_x)
